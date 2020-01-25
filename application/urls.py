6b92c07264a0ab121a2372e107c1df58443b7f66
ruslankhayrut <onsunday1703@gmail.com>
2020-01-25 09:35:56 +0300

    registration

--- application/urls.py
+++ application/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'applicaton'
+app_name = 'application'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('act/', views.act, name='act'),
+
 ]
--- application/views.py
+++ application/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'application/index.html')
 
+def registration(request):
+    return render(request, 'application/registration.html')
+
+
+
+
+def act(request):
+    if '_login' in request.POST:
+        return HttpResponseRedirect(reverse('login'))
+    elif '_register' in request.POST:
+        return HttpResponseRedirect(reverse('application:registration'))
